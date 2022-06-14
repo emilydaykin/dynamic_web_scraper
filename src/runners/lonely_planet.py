e95ec66ba79d5a0f24a8ed7383ac3c82a6928752
Emily Daykin <emilydaykin31@gmail.com>
2022-06-14 11:18:05 +0200

    Added runner file for lonely planet

---
+++ src/runners/lonely_planet.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from scraper.dynamic_scraper import Scraper
+
+
+scraper = Scraper()
+scraper.scrape_lonely_planet_cities()
