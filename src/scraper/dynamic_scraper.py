c536e4ecb9226179b79f9a52395d1db12af0878a
Emily Daykin <emilydaykin31@gmail.com>
2022-06-17 23:57:09 +0200

    fixed imdb scraper method, and added try-catch block to lonely planet scraper method

--- src/scraper/dynamic_scraper.py
+++ src/scraper/dynamic_scraper.py
@@ -19,54 +19,51 @@ class Scraper:
     def scrape_lonely_planet_cities(self, urls: List[str] = URLs_lonely_planet) -> List[dict]:
         cities = []
         for index, city_url in enumerate(urls):
-            # Parse page
-            print(f'{index+1}/{len(urls)}: {city_url}')
-            page = requests.get(city_url)
-            soup = BeautifulSoup(page.content, 'html.parser')
+            try:
+                # Parse page
+                print(f'{index+1}/{len(urls)}: {city_url}')
+                page = requests.get(city_url)
+                soup = BeautifulSoup(page.content, 'html.parser')
 
-            # City
-            city = soup.find_all(
-                'h1', class_='text-3xl font-display md:text-6xl leading-tighter font-semibold font-bold text-blue mb-6 lg:mb-8')[0].text
+                # City
+                city = soup.find_all(
+                    'h1', class_='text-3xl font-display md:text-6xl leading-tighter font-semibold font-bold text-blue mb-6 lg:mb-8')[0].text
 
-            # Continent & Country:
-            location = soup.find_all(
-                'a', class_='transition-colors ease-out cursor-pointer text-black hover:text-blue link-underline')
-            continent = location[0].text
-            country = location[1].text
-            # State or communidad. The US will be populated with states for example, Spain with communidades
-            state = location[2].text if len(location) == 3 else ''
+                # Continent & Country:
+                location = soup.find_all(
+                    'a', class_='transition-colors ease-out cursor-pointer text-black hover:text-blue link-underline')
+                continent = location[0].text
+                country = location[1].text
+                # State or communidad. The US will be populated with states for example, Spain with communidades
+                state = location[2].text if len(location) == 3 else ''
 
-            # Description
-            description = soup.find_all(
-                'div', class_='readMore_content___5EuR relative overflow-hidden max-h-96 is-max wysiwyg')[0].p.text
+                # Description
+                description = soup.find_all(
+                    'div', class_='readMore_content___5EuR relative overflow-hidden max-h-96 is-max wysiwyg')[0].p.text
 
-            # Top 3 attractions (names):
-            top_3_attractions = [x.text for x in soup.find_all(
-                'a', class_='text-xl font-semibold')[:3]]
+                # Top 3 attractions (names):
+                top_3_attractions = [x.text for x in soup.find_all(
+                    'a', class_='text-xl font-semibold')[:3]]
 
-            # Cover image
-            image = soup.find_all('meta')[10]['content']
+                # Cover image
+                image = soup.find_all('meta')[10]['content']
 
-            print(f'-------{city} scraped.')
+                print(f'------- {city} scraped.')
 
-            fields = {
-                'city': city,
-                'country': country,
-                'state': state,
-                'continent': continent,
-                'description': description,
-                'top_3_attractions': top_3_attractions,
-                'image': image
-            }
+                fields = {
+                    'city': city,
+                    'country': country,
+                    'state': state,
+                    'continent': continent,
+                    'description': description,
+                    'top_3_attractions': top_3_attractions,
+                    'image': image
+                }
 
-            # if seed_data:
-            #     cities.append({
-            #         'model': 'cities.city',
-            #         'pk': index + 3,
-            #         'fields': fields
-            #     })
-            # else:
-            cities.append(fields)
+                cities.append(fields)
+
+            except Exception as err:
+                print(f'Error "{err}" for the URL {city_url}')
 
         return cities
 
@@ -149,7 +146,7 @@ class Scraper:
                                          class_='ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link')[
                     -7].text
 
-                print(f'\t{title} scraped.')
+                print(f'------- {title} scraped.')
 
                 pilot_year, finale_year = self._extract_years(years)
 
@@ -168,10 +165,11 @@ class Scraper:
 
                 all_series.append(series_object)
 
-                return all_series
-
             except Exception as err:
                 print(f'Error "{err}" for the URL {url}')
+
+        return all_series
+
 
     def convert_scraped_results_to_json_file(self, data: List[dict], file_name: str):
         with open(f"{file_name}.json", "w") as outfile:
