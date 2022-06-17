2a7044c30eed83034c7cf298ead918d941c92b94
Emily Daykin <emilydaykin31@gmail.com>
2022-06-17 23:57:50 +0200

    added imdb runner script

---
+++ src/runners/imdb.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from scraper.dynamic_scraper import Scraper
+
+scraper = Scraper()
+series = scraper.scrape_imdb()
+# print(series)
+
+
