748a1468bac9f681ec75308bfa5084a0209e8a55
Emily Daykin <emilydaykin31@gmail.com>
2022-06-18 00:00:30 +0200

    added imdb data

--- src/data/urls.py
+++ src/data/urls.py
@@ -32,4 +32,40 @@ URLs_lonely_planet = [
 ]
 
 
-URLs_imdb = []
+URLs_imdb = [
+    'https://www.imdb.com/title/tt2741602/?ref_=fn_al_tt_1',  # The Blacklist
+    'https://www.imdb.com/title/tt1442437/?ref_=fn_al_tt_1',  # Modern Family
+    'https://www.imdb.com/title/tt1839578/?ref_=fn_al_tt_1',  # Person of Interest
+    'https://www.imdb.com/title/tt6470478/?ref_=fn_al_tt_1',  # The Good Doctor
+    'https://www.imdb.com/title/tt1475582/?ref_=fn_al_tt_1',  # Sherlock
+    'https://www.imdb.com/title/tt5511582/?ref_=fn_al_tt_1',  # Timeless
+    'https://www.imdb.com/title/tt1843230/?ref_=fn_al_tt_1',  # Once Upon a Time
+    'https://www.imdb.com/title/tt2575988/?ref_=nv_sr_srsg_0',  # Silicon Valley
+    'https://www.imdb.com/title/tt2467372/?ref_=nv_sr_srsg_0',  # Brooklyn Nine-Nine
+    'https://www.imdb.com/title/tt1632701/?ref_=fn_al_tt_1',  # Suits
+    'https://www.imdb.com/title/tt8420184/?ref_=fn_al_tt_1',  # The Last Dance
+    'https://www.imdb.com/title/tt0386676/?ref_=fn_al_tt_1',  # The Office,
+    'https://www.imdb.com/title/tt5753856/?ref_=nv_sr_srsg_6',  # Dark
+    'https://www.imdb.com/title/tt6468322/?ref_=fn_al_tt_1',  # La casa de Papel
+    'https://www.imdb.com/title/tt0108778/?ref_=fn_al_tt_1',  # Friends,
+    'https://www.imdb.com/title/tt2442560/?ref_=fn_al_tt_1',  # Peaky Blinders
+    'https://www.imdb.com/title/tt10986410/?ref_=fn_al_tt_1',  # Ted Lasso,
+    'https://www.imdb.com/title/tt3825328/?ref_=nv_sr_srsg_7',  # Bajo Sospecha
+    'https://www.imdb.com/title/tt0158552/?ref_=nv_sr_srsg_0',  # Charmed
+    'https://www.imdb.com/title/tt0321021/?ref_=nv_sr_srsg_0',  # Without a Trace
+    'https://www.imdb.com/title/tt4955642/?ref_=fn_al_tt_1',  # The Good Place
+    'https://www.imdb.com/title/tt1578873/?ref_=fn_al_tt_1',  # Pretty Little Liars
+    'https://www.imdb.com/title/tt11640018/?ref_=fn_al_tt_1',  # La Brea
+    'https://www.imdb.com/title/tt0460649/?ref_=fn_al_tt_1',  # How i met your mother
+    'https://www.imdb.com/title/tt10048342/?ref_=nv_sr_srsg_0',  # The Queen's Gambit
+    'https://www.imdb.com/title/tt0397442/?ref_=fn_al_tt_1',  # Gossip Girl
+    # The Woman in the house across the street from the girl in the window
+    'https://www.imdb.com/title/tt13315324/?ref_=nv_sr_srsg_0',
+    'https://www.imdb.com/title/tt2100976/?ref_=fn_al_tt_1',  # Impractical Jokers
+    'https://www.imdb.com/title/tt4158110/?ref_=fn_al_tt_2',  # Mr. Robot
+    'https://www.imdb.com/title/tt3920596/?ref_=fn_al_tt_1',  # Big Little Lies
+    'https://www.imdb.com/title/tt10166622/?ref_=fn_al_tt_1',  # The Dropout
+    'https://www.imdb.com/title/tt4270492/?ref_=nv_sr_srsg_0',  # Billions
+    'https://www.imdb.com/title/tt2085059/?ref_=nv_sr_srsg_0',  # Black Mirror
+    'https://www.imdb.com/title/tt1358522/?ref_=fn_al_tt_1',  # White Collar
+]
