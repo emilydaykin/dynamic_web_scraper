a8852d73a5b252f4ac66521954465b66592a0ab4
Emily Daykin <emilydaykin31@gmail.com>
2022-06-19 19:22:40 +0200

    added search runner for imdb

--- src/runners/imdb.py
+++ src/runners/imdb.py
@@ -7,9 +7,23 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from scraper.dynamic_scraper import Scraper
+from data.series import series_to_search
 
+# Instantiate scraper:
 scraper = Scraper()
-series = scraper.scrape_imdb()
+
+# Get series data from IMDb:
+# series = scraper.scrape_imdb()
 # print(series)
 
+# Search for some series and get results (url):
+urls_to_scrape = []
+for series in series_to_search:
+    print(f'searching {series}...')
+    url_results = scraper.scrape_imdb_search(series)
+    urls_to_scrape.append(url_results)
+print(urls_to_scrape)
 
+# Convert series dictionary into dataframe:
+
+# Export series dictionary into json file:
