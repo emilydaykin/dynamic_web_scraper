e6777282029afecdfa5a622f1e81427ffe6eb3f5
Emily Daykin <emilydaykin31@gmail.com>
2022-06-20 14:43:57 +0200

    scraper helper function (year split) tested

--- src/scraper/tests/test_scraper/test_static_scraper.py
+++ src/scraper/tests/test_scraper/test_static_scraper.py
@@ -3,6 +3,7 @@ import re
 
 from scraper.dynamic_scraper import Scraper
 
+
 @pytest.fixture
 def mock_series_urls():
     return [
@@ -10,6 +11,7 @@ def mock_series_urls():
         'https://www.imdb.com/title/tt1632701/?ref_=fn_al_tt_1',  # Suits
         'https://www.imdb.com/title/tt8420184/?ref_=fn_al_tt_1'  # The Last Dance
     ]
+
 
 def test_static_imdb(mock_series_urls):
     scraper = Scraper()
@@ -31,6 +33,29 @@ def test_static_imdb(mock_series_urls):
         assert bool(re.match(r'^-?\d+(?:\.\d+)$', show['rating'])), 'Series rating is not a float.'
     assert 'Brooklyn Nine-Nine' in series_titles, 'Brooklyn Nine-Nine not scraped.'
     assert any('Suits' in show_title for show_title in series_titles), 'Suites not scraped.'
+
+
+def test_imdb_year_split():
+    """Testing helper function"""
+    scraper = Scraper()
+
+    pilot_year, finale_year = scraper._extract_years('2013–2017')
+    assert pilot_year.isnumeric(), 'Series first year from split is not a number.'
+    assert pilot_year == '2013'
+    assert finale_year.isnumeric(), 'Series second year from split is not a number.'
+    assert finale_year == '2017'
+
+    pilot_year, finale_year = scraper._extract_years('2021')
+    assert pilot_year.isnumeric(), 'Series first year from split is not a number.'
+    assert pilot_year == '2021'
+    assert finale_year.isnumeric() == False, "Series second year from split is a number."
+    assert finale_year == 'ongoing', "Series second year from split should be 'ongoing'."
+
+    pilot_year, finale_year = scraper._extract_years('2019–')
+    assert pilot_year.isnumeric(), 'Series first year from split is not a number.'
+    assert pilot_year == '2019'
+    assert finale_year.isnumeric() == False, "Series second year from split is a number."
+    assert finale_year == '', "Series second year from split should be an empty string."
 
 
 def test_static_lonely_planet():
