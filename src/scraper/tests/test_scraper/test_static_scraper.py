f8b5720f454300b346ab72182b1fb297bd63de7a
Emily Daykin <emilydaykin31@gmail.com>
2022-06-20 16:08:39 +0200

    Added fixture, made code more readable and less repetitive

--- src/scraper/tests/test_scraper/test_static_scraper.py
+++ src/scraper/tests/test_scraper/test_static_scraper.py
@@ -12,6 +12,12 @@ def mock_series_urls():
         'https://www.imdb.com/title/tt8420184/?ref_=fn_al_tt_1'  # The Last Dance
     ]
 
+
+@pytest.fixture
+def mock_years_data():
+    return ['2013–2017', '2021', '2019–']
+
+
 @pytest.fixture
 def mock_destinations_urls():
     return [
@@ -27,44 +33,46 @@ def test_static_imdb(mock_series_urls):
     series = scraper.scrape_imdb_series(mock_series_urls)
     assert type(series) == list
     assert len(series) == 3
+
     series_titles = []
     for show in series:
         assert all(key in show.keys() for key in [
             'name', 'genre', 'description', 'actors', 'pilotYear', 'finaleYear', 'rating', 'image'
         ]), 'Key(s) missing.'
         series_titles.append(show['name'])
-        assert type(show['name']) == str, 'Series name is not a string.'
-        assert type(show['genre']) == list, 'Series genre is not a list.'
-        assert type(show['description']) == str, 'Series description is not a string.'
-        assert type(show['actors']) == list, 'Series actors not a list.'
-        assert show['pilotYear'].isnumeric(), 'Series pilot year is not a number.'
-        assert show['episodes'].isnumeric(), 'Series episodes not a number.'
+        for key in ['name', 'description']:
+            assert type(show[key]) == str, f'Series {key} is not a string.'
+        for key in ['genre', 'actors']:
+            assert type(show[key]) == list, f'Series {key} is not a list.'
+        for key in ['pilotYear', 'episodes']:
+            assert show[key].isnumeric(), f'Series {key} not a number.'
+        assert (show['finaleYear'].isnumeric() or type(show['finaleYear']) == str), \
+            'Series finale year is not the right type.'
         assert bool(re.match(r'^-?\d+(?:\.\d+)$', show['rating'])), 'Series rating is not a float.'
+
     assert 'Brooklyn Nine-Nine' in series_titles, 'Brooklyn Nine-Nine not scraped.'
     assert any('Suits' in show_title for show_title in series_titles), 'Suites not scraped.'
 
 
-def test_imdb_year_split():
+def test_imdb_year_split(mock_years_data):
     """Testing helper function"""
     scraper = Scraper()
 
-    pilot_year, finale_year = scraper._extract_years('2013–2017')
-    assert pilot_year.isnumeric(), 'Series first year from split is not a number.'
-    assert pilot_year == '2013'
-    assert finale_year.isnumeric(), 'Series second year from split is not a number.'
-    assert finale_year == '2017'
-
-    pilot_year, finale_year = scraper._extract_years('2021')
-    assert pilot_year.isnumeric(), 'Series first year from split is not a number.'
-    assert pilot_year == '2021'
-    assert finale_year.isnumeric() == False, "Series second year from split is a number."
-    assert finale_year == 'ongoing', "Series second year from split should be 'ongoing'."
-
-    pilot_year, finale_year = scraper._extract_years('2019–')
-    assert pilot_year.isnumeric(), 'Series first year from split is not a number.'
-    assert pilot_year == '2019'
-    assert finale_year.isnumeric() == False, "Series second year from split is a number."
-    assert finale_year == '', "Series second year from split should be an empty string."
+    for mock_years in mock_years_data:
+        pilot_year, finale_year = scraper._extract_years(mock_years)
+        assert pilot_year.isnumeric(), 'Series first year from split is not a number.'
+        if mock_years == '2013–2017':
+            assert pilot_year == '2013'
+            assert finale_year.isnumeric(), 'Series second year from split is not a number.'
+            assert finale_year == '2017'
+        elif mock_years == '2021':
+            assert pilot_year == mock_years
+            assert not finale_year.isnumeric(), "Series second year from split is a number."
+            assert finale_year == 'ongoing', "Series second year from split should be 'ongoing'."
+        elif mock_years == '2019–':
+            assert pilot_year == '2019'
+            assert not finale_year.isnumeric(), "Series second year from split is a number."
+            assert finale_year == '', "Series second year from split should be an empty string."
 
 
 def test_static_lonely_planet(mock_destinations_urls):
